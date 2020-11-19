a72cc8da743056d1d6f6bd18531a0cd6b45e8936
Leonardo Favio Romero Silva <leo@MacBook-Pro-de-Leonardo.local>
2020-11-18 20:52:52 -0500

    actualizacion

--- src/main/java/com/reto/mudanzas/MudanzasApiApplication.java
+++ src/main/java/com/reto/mudanzas/MudanzasApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MudanzasApiApplication {
 
 	public static void main(String[] args) {
+            
 		SpringApplication.run(MudanzasApiApplication.class, args);
 	}
 
