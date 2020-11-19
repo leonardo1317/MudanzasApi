9eb3e910eb59921dab7c04a8121d0774b8c61921
Leonardo Favio Romero Silva <leo@MacBook-Pro-de-Leonardo.local>
2020-11-18 20:44:06 -0500

    Se crea el proyecto

---
+++ src/main/java/com/reto/mudanzas/MudanzasApiApplication.java
@@ -0,0 +1,13 @@
+package com.reto.mudanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudanzasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudanzasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/mudanzas/MudanzasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.mudanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudanzasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
